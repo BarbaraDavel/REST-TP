71b79532e7da664795ebd15623666aefc47b07c4
Your Name <you@example.com>
2021-03-10 17:48:54 -0300

    ....

---
+++ app/Usuario.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    protected $table = "usuario";
+    use HasFactory;
+}
