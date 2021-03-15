b8556412631d64289252dade04fd4e66a385c249
Santiago-Rivero <santiagonrivero@gmail.com>
2021-03-15 17:20:40 -0300

    avance del front

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -19,5 +19,77 @@
             </div>
         </div>
     </div>
+</div> -->
+
+<div class="container-md">
+    <div class="row">
+        <div class="col-sm">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img type="image/jpg" src="https://pbs.twimg.com/profile_images/1171201523705110539/q85a1XXX_400x400.jpg" class="d-block w-100" alt="Como empecé"
+                    width="800px" height="350px">
+                    <!--https://pbs.twimg.com/profile_images/1171201523705110539/q85a1XXX_400x400.jpg -->
+                </div>
+                <div class="carousel-item">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrqytySnAj1ONaJNVlzrg50uipUhuSr-s0Kg&usqp=CAU" class="d-block w-100" alt="como voy"
+                    width="800px" height="350px">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://i.pinimg.com/originals/69/d7/45/69d745f469c030dadd0293bd48d36a21.jpg" class="d-block w-100" alt="Como voy a terminar"
+                    width="800px" height="350px">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden" >Next</span>
+            </button>
+            </div>   
+        </div>
+    </div>
+    <div class="container emprendimientos mt-4">
+        <h3>Emprendimientos</h3>
+        <div class="row mt-4">
+            <div class="col-md-6 col-lg-3">
+                <p>Emprendimiento 1</p>
+                <img alt="" class="img-fluid">
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <p>Emprendimiento 2</p>
+                <img alt="" class="img-fluid">
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <p>Emprendimiento 3</p>
+                <img alt="" class="img-fluid">
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <p>Emprendimiento 3</p>
+                <img alt="" class="img-fluid">
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-6 col-lg-3">
+                <p>Emprendimiento 1</p>
+                <img alt="" class="img-fluid">
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <p>Emprendimiento 2</p>
+                <img alt="" class="img-fluid">
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <p>Emprendimiento 3</p>
+                <img alt="" class="img-fluid">
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <p>Emprendimiento 3</p>
+                <img alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
 </div>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,83 +1,83 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'REST') }}</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    </head>
+    <body class="d-flex flex-column h-100">
+        <div class="">
+        <div class="row">
+                <div id="app">
+                    <nav class="navbar navbar-expand-md navbar-dark shadow-sm mi-nav">
+                        <div class="col-3 ">
+                            <a class="navbar-brand" href="{{ url('/') }}"><span class="mi_span"><img class="logo" href="#"/></span></a>
+                        </div>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                            <span class="navbar-toggler-icon"></span>
+                            </button>
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        
+                                <ul class="navbar-nav mr-auto">
+                                
+                                </ul>    
+                                <div class="login" class="col-3">     
+                                    <ul  class="navbar-nav ml-auto">
+                                        @guest
+                                            @if (Route::has('login'))
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Log In') }}</a>
+                                                </li>
+                                            @endif
+                                        
+                                            @if (Route::has('register'))
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Sign In') }}</a>
+                                                </li>
+                                            @endif
+                                            @else
+                                                <li class="nav-item dropdown">
+                                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                                        {{ Auth::user()->name }}
+                                                    </a>
 
-    <title>{{ config('app.name', 'REST') }}</title>
+                                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                                        onclick="event.preventDefault();       
+                                                        document.getElementById('logout-form').submit();">
+                                                        {{ __('Desloguear') }}
+                                                    </a>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'REST') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                     
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Ingresar') }}</a>
-                                </li>
-                            @endif
-                            
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Desloguear') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
+                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                    @csrf
+                                                    </form>
+                                                    </div>
+                                                </li>
+                                        @endguest
+                                    </ul>
                                 </div>
-                            </li>
-                        @endguest
-                    </ul>
+                            </div>
+                    </nav>
                 </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+        </div>
+        </div> 
+            <main class=" d-flex flex-column h-100 py-4">
+                @yield('content')
+            </main>
+            <footer class="mt-auto w-100 mi-footer">
+                <h3>NOSOTROS</h3>
+				<div class="row justify-content-center mb-3">
+					
+				</div>
+                
+            </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    </body>
 </html>
