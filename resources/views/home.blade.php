4a2abd6d0fc2fb8cb0b4e2039681150f437fbf8e
Your Name <you@example.com>
2021-03-15 11:22:09 -0300

    modificación del header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,15 +35,15 @@
             <div class="carousel-inner">
             <div class="carousel-item active">
                 <img src="https://pbs.twimg.com/profile_images/1171201523705110539/q85a1XXX_400x400.jpg " class="d-block w-100" alt="Como empecé"
-                width="100%" height="500px">
+                width="1000px" height="500px">
             </div>
             <div class="carousel-item">
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrqytySnAj1ONaJNVlzrg50uipUhuSr-s0Kg&usqp=CAU" class="d-block w-100" alt="como voy"
-                width="100%" height="500px">
+                width="1000px" height="500px">
             </div>
             <div class="carousel-item">
                 <img src="https://i.pinimg.com/originals/69/d7/45/69d745f469c030dadd0293bd48d36a21.jpg" class="d-block w-100" alt="Como voy a terminar"
-                width="100%" height="500px">
+                width="1000px" height="500px">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
@@ -52,14 +52,13 @@
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
+            <span class="visually-hidden" >Next</span>
         </button>
         </div>
     </div>
-
-
-
+                                                              
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
   </body>
 </html>
+
 @endsection
